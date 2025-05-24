70f19229b3f7c0e4b20b7e6290a5522451a0ab37
dattnn <dattnnse173524@fpt.edu.vn>
2025-05-24 16:39:48 +0700

    fix : register

--- src/main/java/com/example/lovenhavestopsystem/user/crud/dto/request/AccountRegisterDTO.java
+++ src/main/java/com/example/lovenhavestopsystem/user/crud/dto/request/AccountRegisterDTO.java
@@ -1,5 +1,9 @@
 package com.example.lovenhavestopsystem.user.crud.dto.request;
 
+import com.example.lovenhavestopsystem.core.base.BaseMessage;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +12,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AccountRegisterDTO {
+    @NotNull(message = BaseMessage.PASSWORD_NOT_NULL)
+    @Size(min = 8, message = BaseMessage.PASSWORD_MIN_LENGTH)
     private String password;
+    @NotNull(message = BaseMessage.EMAIL_NOT_NULL)
+    @Email(message = BaseMessage.EMAIL_INVALID)
     private String email;
 }
