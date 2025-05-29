205e75b05b492c9f72fc92449a44ceaa5553dba9
dattnn <dattnnse173524@fpt.edu.vn>
2025-05-29 16:04:49 +0700

    add : blog

---
+++ src/main/java/com/example/lovenhavestopsystem/model/entity/BlogContent.java
@@ -0,0 +1,14 @@
+package com.example.lovenhavestopsystem.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BlogContent {
+    private String title;
+    private String detail;
+}
+
