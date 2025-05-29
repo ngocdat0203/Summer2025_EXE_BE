13ae2b7e271b26b93e7b3c1e8a49bf7b5963e9e1
dattnn <dattnnse173524@fpt.edu.vn>
2025-05-29 20:11:56 +0700

    add : blog

---
+++ src/main/java/com/example/lovenhavestopsystem/dto/request/BlogRequestDTO.java
@@ -0,0 +1,19 @@
+package com.example.lovenhavestopsystem.dto.request;
+
+import com.example.lovenhavestopsystem.model.entity.BlogContent;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BlogRequestDTO {
+    private String title;
+    private String headline;
+    private List<BlogContent> content;
+    private String summary;
+    private int accountId;
+}
