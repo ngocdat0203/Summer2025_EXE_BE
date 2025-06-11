6bbd2b27e653dd7d39b8752487b87a91600090a9
dattnn <dattnnse173524@fpt.edu.vn>
2025-06-11 21:35:37 +0700

    fix : jwt

--- src/main/java/com/example/lovenhavestopsystem/user/auth/jwt/JwtService.java
+++ src/main/java/com/example/lovenhavestopsystem/user/auth/jwt/JwtService.java
@@ -42,7 +42,7 @@ public class JwtService {
     }
 
 
-    public String generateToken(int id, String email, List<RoleName> roles, String fullName, String address) {
+    public String generateToken(int id, String email, List<RoleName> roles, String fullName, String address, int consultantId) {
 
 
         Map<String, Object> claims = new HashMap<>();
@@ -54,6 +54,7 @@ public class JwtService {
                 .claim("roles", roles)
                 .claim("fullName", fullName)
                 .claim("address", address)
+                .claim("counsultantId", consultantId)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours
                 .signWith(getKey(), SignatureAlgorithm.HS256).compact();
