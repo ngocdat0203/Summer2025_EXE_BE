11ae7bced95bd8ae6f3845f04831e440c08ace17
annt1303 <anngtr1303@gmail.com>
2025-06-11 08:54:16 +0700

    add payment function

---
+++ src/main/java/com/example/lovenhavestopsystem/controller/VNPayController.java
@@ -0,0 +1,32 @@
+package com.example.lovenhavestopsystem.controller;
+
+import com.example.lovenhavestopsystem.core.base.BaseMessage;
+import com.example.lovenhavestopsystem.core.base.BaseResponse;
+import com.example.lovenhavestopsystem.service.inter.IVNPayService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/vn-pay")
+public class VNPayController {
+    private final IVNPayService service;
+
+    public VNPayController(IVNPayService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/pay")
+    public ResponseEntity<BaseResponse<?>> pay(@RequestParam int appointmentId, @RequestParam String returnUrl, HttpServletRequest request) {
+        return ResponseEntity.ok(new BaseResponse<>(HttpStatus.OK.value(), BaseMessage.GET_SUCCESS, service.pay(appointmentId, returnUrl, request)));
+    }
+
+    @GetMapping("/deposit")
+    public ResponseEntity<BaseResponse<?>> deposit(@RequestParam int appointmentId, @RequestParam String returnUrl, HttpServletRequest request) {
+        return ResponseEntity.ok(new BaseResponse<>(HttpStatus.OK.value(), BaseMessage.GET_SUCCESS, service.deposit(appointmentId, returnUrl, request)));
+    }
+}
--- src/main/java/com/example/lovenhavestopsystem/service/imple/AppointmentService.java
+++ src/main/java/com/example/lovenhavestopsystem/service/imple/AppointmentService.java
@@ -65,4 +65,8 @@ public class AppointmentService implements IAppointmentService {
         appointmentRepository.save(appointment);
     }
 
+    @Override
+    public List<Appointment> getAllAppointments() {
+        return appointmentRepository.findAll();
+    }
 }
