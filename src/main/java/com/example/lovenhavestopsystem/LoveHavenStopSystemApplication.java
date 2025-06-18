317ef8d0372195086523d75654072f7f9990eac5
dattnn <dattnnse173524@fpt.edu.vn>
2025-06-18 16:11:51 +0700

    add : dockerfile

--- src/main/java/com/example/lovenhavestopsystem/LoveHavenStopSystemApplication.java
+++ src/main/java/com/example/lovenhavestopsystem/LoveHavenStopSystemApplication.java
@@ -3,6 +3,7 @@ package com.example.lovenhavestopsystem;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.crypto.SecretKey;
 import java.security.NoSuchAlgorithmException;
@@ -18,15 +19,35 @@ public class LoveHavenStopSystemApplication {
                 .load();
         dotenv.entries().forEach(entry ->
                 System.setProperty(entry.getKey(), entry.getValue())
+
+
         );
+
+
+        String rawPassword = "0979560506D@at"; // Mật khẩu gốc
+        String encodedPassword = encodePassword(rawPassword);
+        System.out.println("Mật khẩu đã mã hóa: " + encodedPassword);
+
+
+
+
         SpringApplication.run(LoveHavenStopSystemApplication.class, args);
        /* BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
         String raw = "password123";
         String hash = encoder.encode(raw);
 
         System.out.println("Hash: " + hash);
+
         System.out.println("Match? " + encoder.matches(raw, hash));*/
 
+
+
+    }
+
+
+    public static String encodePassword(String rawPassword) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder.encode(rawPassword);
     }
 
 }
