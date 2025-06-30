0b116b89760f8570f41455ca71f40c6da0e246f5
dattnn <dattnnse173524@fpt.edu.vn>
2025-06-30 16:52:30 +0700

    fix : entity

--- src/main/java/com/example/lovenhavestopsystem/mapper/ChatMessageMapper.java
+++ src/main/java/com/example/lovenhavestopsystem/mapper/ChatMessageMapper.java
@@ -3,15 +3,24 @@ package com.example.lovenhavestopsystem.mapper;
 import com.example.lovenhavestopsystem.dto.request.ChatMessageRequestDTO;
 import com.example.lovenhavestopsystem.dto.response.ChatMessageResponseDTO;
 import com.example.lovenhavestopsystem.model.entity.ChatMessage;
+import org.mapstruct.Context;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ChatMessageMapper {
-    ChatMessageResponseDTO toChatMessageResponse(ChatMessage chatMessage);
+
+    @Mapping(source = "chatMessage.id", target = "id")
+    @Mapping(source = "chatMessage.message", target = "message")
+    @Mapping(source = "chatMessage.createdDate", target = "createdDate")
+    @Mapping(source = "chatMessage.conversation.id", target = "conversationId")
+    @Mapping(source = "chatMessage.account", target = "sender")
+    @Mapping(target = "me", expression = "java(chatMessage.getAccount().getEmail().equalsIgnoreCase(currentUserGmail.trim()))")
+    ChatMessageResponseDTO toChatMessageResponse(ChatMessage chatMessage, @Context String currentUserGmail);
 
     ChatMessage toChatMessage(ChatMessageRequestDTO request);
 
-    List<ChatMessageResponseDTO> toChatMessageResponses(List<ChatMessage> chatMessages);
+    List<ChatMessageResponseDTO> toChatMessageResponses(List<ChatMessage> chatMessages, @Context String currentUserGmail);
 }
